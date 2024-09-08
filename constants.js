dd1dcd6c82a1cde06de0ce636fdd4a775aae59a0
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-07 23:25:25 -0300

    Corrigir cor Sobrepeso

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 export const categoriasIMC = [
     { categoria: "Abaixo do peso", min: 0, max: 18.4, cor: "orange" },
     { categoria: "Peso normal", min: 18.5, max: 24.9, cor: "green" },
-    { categoria: "Sobrepeso", min: 25, max: 29.9, cor: "yellow" },
+    { categoria: "Sobrepeso", min: 25, max: 29.9, cor: "#f1c40f" },
     { categoria: "Obesidade", min: 30, max: 39.9, cor: "red" },
     { categoria: "Obesidade grave", min: 40, max: 100, cor: "darkred" }
 ];
